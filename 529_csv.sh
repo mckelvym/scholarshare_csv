a0db0d3f3a0f277066939a20a455aee9e0ff3b0e
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2024-01-09 07:32:42 -0500

    529_csv.sh update for paths

--- 529_csv.sh
+++ 529_csv.sh
@@ -11,17 +11,14 @@ cd "$path"
 merge_name=dailyprices.csv
 out_name=dailyprices_tmp.csv
 out_rss_name=feed.xml
-archive_name=dailyprices`date "+%Y%m%d"`.csv
 docker run -u $(id -u):$(id -g) -v "$PWD":/data registry.hub.docker.com/mckelvym/scholarshare.price:1.6.1 --merge-file=/data/$merge_name --out-file=/data/$out_name --out-rss-file=/data/$out_rss_name
 if [ -s $out_name ] ; then
     mv $out_name $merge_name
-    cp -v $merge_name $archive_name
 
-    cd scholarshare_prices && \
-	cp ../$out_rss_name . && \
-        cp ../$merge_name . && \
-        git add -u && \
-        git commit -m "Daily update $archive_name" && \
+    date=`date "+%Y%m%d"`
+    git add -u && \
+        git commit -m "Daily update $date" && \
+	git pull && \
         git push
 
     head $merge_name
